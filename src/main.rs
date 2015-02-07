43d044bff28860da5b686a3db99f864ecb322408
Zach Latta <zach@zachlatta.com>
2015-02-07 15:30:25 -0800

    Make an HTTP request.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate hyper;
+
 fn main() {
-    println!("Hello, world!");
+  let mut client = Client::new();
+
+  let res = client.get("http://www.google.com")
+    .header(Connection(vec![Close]))
+    .send().unwrap();
+
+  let body = res.read_to_string().unwrap();
+
+  println!("Response: {}", body);
 }
