43e37d276582dbe18699c497687b08e1bf63e624
Zach Latta <zach@zachlatta.com>
2015-02-08 15:52:02 +0000

    working server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,25 @@
-extern crate hyper;
-extern crate iron;
+#[macro_use]
 extern crate rustless;
 
-use hyper::status::StatusCode;
-use iron::Iron;
-use rustless::{
-    Application, Api, Nesting, Versioning
-};
+extern crate iron;
+
+use rustless::{Nesting};
 
 fn main() {
-    let api = Api::build(|api| {
+    let app = rustless::Application::new(rustless::Api::build(|api| {
         api.prefix("api");
-    });
+        api.version("v1", rustless::Versioning::Path);
+
+        api.namespace("jobs", |jobs_ns| {
+            jobs_ns.get("server_status", |endpoint| {
+                endpoint.handle(|client, _params| {
+                    client.text("Everything is OK\n".to_string())  
+                })
+            });
+        })
+    }));
+
+    iron::Iron::new(app).listen("localhost:4000").unwrap();
+    println!("On 4000");
+
 }
